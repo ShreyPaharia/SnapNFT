f6d3e03b0611d270b311121c4e94f426a26bbed1
Mahantesh Surgihalli <mSurgihalli@collegeboard.org>
2021-08-16 06:34:05 -0600

    screen shot of filter page

--- packages/react-app/src/views/FilterCard.jsx
+++ packages/react-app/src/views/FilterCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import { Card, Modal, Table } from "antd";
 import { AppCanvas } from "../helpers"
 
@@ -6,13 +6,29 @@ import { AppCanvas } from "../helpers"
 // import ethereumIcon from '@iconify-icons/mdi/ethereum';
 import '../styles/CardAnt.css';
 import { useHistory } from 'react-router-dom'
+import { useScreenshot, createFileName } from "use-react-screenshot";
 const { Meta } = Card;
 function FilterCard({ nft }) {
     const { contentURI, imageURL, metadataURI } = nft;
     const { name, description } = metadataURI
     const [visible, setVisible] = useState(false);
-
-    console.log("nft", nft);
+    const ref = createRef(null);
+    const [image, takeScreenShot] = useScreenshot({
+      type: "image/jpeg",
+      quality: 1.0
+    });
+  
+    const download = (image, { name = "img", extension = "jpg" } = {}) => {
+      const a = document.createElement("a");
+      a.href = image;
+      a.download = createFileName(extension, name);
+      a.click();
+    };
+  
+    const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+  
+  
+    // console.log("nft", nft);
     
     return (
         <div onClick={()=>setVisible(true)}>
@@ -27,15 +43,23 @@ function FilterCard({ nft }) {
               title="Filter Preview"
               centered
               visible={visible}
-              onOk={() => { setVisible(false)
-                        //   submitOnConfirmation();
-                        // getImage()
-                      }}
-              onCancel={() => setVisible(false)}
+              onOk={() => { setVisible(false);
+                takeScreenShot(ref.current);
+                downloadScreenshot();
+
+              }}
+              onCancel={() => {setVisible(false);
+              }}
               okText="Save"
-              // cancelText="Ciao"
+              cancelText="Cancel"
               width={1000}>
-                <AppCanvas filterSrc={`${contentURI}/filter.js`} rootPath={`${contentURI}`}></AppCanvas>
+                <div  ref={ref}         style={{
+                                      border: "1px solid #ccc",
+                                      padding: "10px",
+                                      marginTop: "20px"
+                                    }}>
+                  <AppCanvas filterSrc={`${contentURI}/filter.js`} rootPath={`${contentURI}`}></AppCanvas>
+                </div>
           </Modal>
         </div>
         )
